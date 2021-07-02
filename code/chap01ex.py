9f2b4d0f96ef1b5c9f8db278f488461ef07bf3da
nouryousry <nourhan149751@bue.edu.eg>
2021-07-02 13:35:35 +0200

    solving exercises- chapter 1
    Solving exercises

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -13,12 +13,30 @@ import sys
 import nsfg
 import thinkstats2
 
+def read_respondent(dct_file='2002FemResp.dct',
+                    dat_file='2002FemResp.dat.gz',
+                    nrows=None):
+        """Reads the NSFG respondent data.
+
+        dct_file: string file name
+        dat_file: string file name
+
+        returns: DataFrame
+        """
+        dct = thinkstats2.ReadStataDct(dct_file)
+        df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+        nsfg.CleanFemResp(df)
+        return df
 
 def main(script):
     """Tests the functions in this module.
 
     script: string script name
     """
+    femresp_df=read_respondent()
+    print(femresp_df.pregnum.value_counts())
+    femresp_dict=nsfg.MakePregMap(femresp_df)
+
     print('%s: All tests passed.' % script)
 
 
